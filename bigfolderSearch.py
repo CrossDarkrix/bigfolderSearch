b984f8ec6f33b2ff4e528d5b5f3c0a602439ffd1
CrossDarkrix <crosseddarkrix@gmail.com>
2026-01-28 11:09:40 +0900

    Update bigfolderSearch.py

--- bigfolderSearch.py
+++ bigfolderSearch.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from pathlib import Path
+from operator import itemgetter
 
 def _iterfiles(path, size_min_in_mb):
     for root, dirs, files in os.walk(path):
@@ -38,7 +39,7 @@ def _iterdirs(path, size_min_in_mb=50):
 def main(path, size_min_in_mb):
     try:
         folder_info = _iterdirs(path, size_min_in_mb)
-        for folder in list(set(sorted(folder_info.items(), key=lambda x:x[1]))):
+        for folder in list(set(sorted(folder_info.items(), key=itemgetter(1), reverse=True))):
             folder_name, size = folder
             if float(size) >= 20.0:
                 print('{}: {:.1f}MB'.format(folder_name, size))
