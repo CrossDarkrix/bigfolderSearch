8a3c075a0322eeb4f743a09d30e23813f8b7eeca
CrossDarkrix <crosseddarkrix@gmail.com>
2026-01-28 10:47:31 +0900

    Update bigfolderSearch.py

--- bigfolderSearch.py
+++ bigfolderSearch.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from pathlib import Path
 
 def _iterfiles(path, size_min_in_mb):
@@ -8,14 +9,16 @@ def _iterfiles(path, size_min_in_mb):
                 p = Path(os.path.join(root, file))
                 try:
                     size = p.stat().st_size
-                except:
+                except PermissionError:
                     size = 0
                 if size >= size_min_in_mb:
                     yield size
-        except:
+                else:
+                    yield 0
+        except PermissionError:
             yield 0
 
-def _iterdirs(path, size_min_in_mb):
+def _iterdirs(path, size_min_in_mb=50):
     _directory_info = {}
     def calc_total(path):
         total = 0
@@ -27,7 +30,7 @@ def _iterdirs(path, size_min_in_mb):
             for directory in dirs:
                 folder = os.path.join(root, directory)
                 _directory_info[folder] = calc_total(folder)
-        except:
+        except PermissionError:
             pass
 
     return _directory_info
@@ -35,11 +38,16 @@ def _iterdirs(path, size_min_in_mb):
 def main(path, size_min_in_mb):
     try:
         folder_info = _iterdirs(path, size_min_in_mb)
-        for folder in reversed(sorted(folder_info.items(), key=lambda x:x[1])):
+        for folder in reversed(list(set(sorted(folder_info.items(), key=lambda x:x[1])))):
             folder_name, size = folder
             if float(size) >= 20.0:
                 print('{}: {:.1f}MB'.format(folder_name, size))
-    except:
-        print("Error")
+    except Exception as Err:
+        print("Error: {}".format(Err))
 
-main(os.getcwd(), 50)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 1 and len(sys.argv) != 0:
+        main(sys.argv[1], 50)
+    else:
+        main(input('Search PATH: ') or os.getcwd(), 50)
